3d2a84094db8f3d4170f59cf9fff62cadabe8c27
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-07-31 18:54:48 +0300

    feat: hover logo

--- components/header.tsx
+++ components/header.tsx
@@ -7,9 +7,11 @@ export const Header = () => {
       <div className="container mx-auto p-3">
         <nav className="flex items-center">
           <Link href="/">
-            <Image src="/logo.png" alt="logo" width="48" height="48"></Image>
+            <div className="cursor-pointer flex items-center">
+              <Image src="/logo.png" alt="logo" width="48" height="48"></Image>
+              <span className="text-2xl text-almond font-semibold ml-2">Bun</span>
+            </div>
           </Link>
-          <span className="text-2xl text-almond font-semibold ml-2">Bun</span>
         </nav>
       </div>
     </header>
